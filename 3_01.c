fd85ebce460c00688ac823b07d1b4e503b0dff8c
김태윤 <kim0517@sju.ac.kr>
2021-07-05 00:40:28 +0900

    해시 / 정렬 분류

--- 3_01.c
+++ 3_01.c
@@ -1,4 +1,4 @@
-/*#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,4 +39,4 @@ int main(int argc, char* argv[]) {
 		printf("해시 테이블 내용:\n");
 		displayHashTable(pHashTable);
 	}
-}*/
+}
