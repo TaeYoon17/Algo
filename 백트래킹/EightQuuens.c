3478b4b688dbcc77caf3f61a9b3ee208e0905eec
김태윤 <kim0517@sju.ac.kr>
2021-07-27 21:14:28 +0900

    Eight Queen 문제

---
+++ 백트래킹/EightQuuens.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define blockCount 8
+
+void displayGrid(int colIdx[]){
+	for (int i = 0; i < blockCount; i++) {
+		for (int j = 0; j < blockCount; j++) {
+			if (j == colIdx[i]) printf("O");
+			else printf("X");
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+int ThisPathOkay(int colIdx[],int row,int newCol) {
+	for (int i = 0; i < row; i++) {
+		if (colIdx[i] == newCol) return 0;
+		int a = i, b = colIdx[a];
+		int rowLen = abs(row - a), colLen = abs(newCol - b);
+		if (rowLen == colLen) return 0;
+	}
+	return 1;
+}
+int isFullRow(int row) {
+	return row == blockCount - 1 ? 1: 0;
+}
+void Queen_BackTracking(int colIdx[],int row) {
+	for (int i = 0; i < blockCount; i++) {
+		if (ThisPathOkay(colIdx,row,i)) {
+			colIdx[row] = i;
+			if (isFullRow(row)) {
+				displayGrid(colIdx);
+			}
+			else {
+				Queen_BackTracking(colIdx, row + 1);
+			}
+		}
+		else {
+			//printf("Already have path");
+			//displayGrid(colIdx);
+		}
+	}
+}
+
+void startQueens(int grid[]) {
+	Queen_BackTracking(grid, 0);
+}
+int main() {
+	int grid[blockCount] = {-1};
+	startQueens(grid);
+	return 0;
+}
--- 백트래킹/tspBackMain.c
+++ 백트래킹/tspBackMain.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #include "tspBackTracking.h"
@@ -13,4 +14,4 @@ int main(void) {
     tsp_Back(costGraph, optimalPath, &minCost);
     printPath(optimalPath);
     return 0;
-}
+}*/
